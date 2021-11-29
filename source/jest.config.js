ef5101fa4092a668463c84edb0386a258b5e9826
Royce <rlabugue@ucsd.edu>
2021-11-29 01:40:25 -0800

    Linting jest.config.js

--- source/jest.config.js
+++ source/jest.config.js
@@ -1,7 +1,7 @@
-module.exports = {
+module.exports = {							// eslint-disable-line
 	preset: 'ts-jest',
 	transform: {
-	  '^.+\\.(ts|tsx)?$': 'ts-jest',
-	  "^.+\\.(js|jsx)$": "babel-jest",
+		'^.+\\.(ts|tsx)?$': 'ts-jest',
+		'^.+\\.(js|jsx)$': 'babel-jest',
 	}
-  };
+};
