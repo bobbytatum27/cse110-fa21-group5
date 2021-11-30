549697e7774066a8ddd5b425d5c780d727a38046
Steven Chang <stevenchang@Stevens-MacBook-Pro.local>
2021-11-29 21:59:43 -0800

    Finish core features

--- source/public/apis/recipeAPIs.js
+++ source/public/apis/recipeAPIs.js
@@ -39,14 +39,14 @@ export async function searchRecipe(params){
 }
 
 /**
- * Search through hundreds of thousands of recipes using advanced filtering and ranking. NOTE: This method combines searching by query, by ingredients, and by nutrients into one endpoint.
+ * Use a recipe id to get full information about a recipe, such as ingredients, nutrition, diet and allergen information, etc.
  * 
- * @param {Object} params A JavaScript object that contains different types of params given in https://spoonacular.com/food-api/docs#Search-Recipes-Complex
- * @return {Object} A JavaScript object that contains all the results in an array
+ * @param {number} id A JavaScript object that contains different types of params given in https://spoonacular.com/food-api/docs#Search-Recipes-Complex
+ * @return {Object} A JavaScript object that contains all the information of the given recipe
 */
-export async function searchRecipe(params){
+export async function getRecipeInformation(id){
 	try {
-		const resp = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonKeysSteven}?query=${params.query}`, {headers: {'Content-Type': 'application/json'}});
+		const resp = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${spoonKeysSteven}`, {headers: {'Content-Type': 'application/json'}});
         console.log(resp.data);
 	} catch (error) {
 		console.error(error)
@@ -54,14 +54,29 @@ export async function searchRecipe(params){
 }
 
 /**
- * Use a recipe id to get full information about a recipe, such as ingredients, nutrition, diet and allergen information, etc.
+ * Search for simple whole foods (e.g. fruits, vegetables, nuts, grains, meat, fish, dairy etc.).
  * 
- * @param {Number} id The id of the recipe.
- * @return {Object} A JavaScript object that contains all the information of the given recipe
+ * @param {string} query The partial or full ingredient name.
+ * @return {Object} A JavaScript object that contains all the information of the query
 */
-export async function searchRecipe(id){
+export async function ingredientsSearch(query){
 	try {
-		const resp = await axios.get(`https://api.spoonacular.com/recipes/${id}/information`, {headers: {'Content-Type': 'application/json'}});
+		const resp = await axios.get(`https://api.spoonacular.com/food/ingredients/search?apiKey=${spoonKeysSteven}?query=${query}`, {headers: {'Content-Type': 'application/json'}});
+        console.log(resp.data);
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+/**
+ * Use an ingredient id to get all available information about an ingredient, such as its image and supermarket aisle.
+ * 
+ * @param {number} id The ingredient id.
+ * @return {Object} A JavaScript object that contains all the information of the given ingredient
+*/
+export async function getIngredientInfo(id){
+	try {
+		const resp = await axios.get(`https://api.spoonacular.com/food/ingredients/${id}/information?apiKey=${spoonKeysSteven}`, {headers: {'Content-Type': 'application/json'}});
         console.log(resp.data);
 	} catch (error) {
 		console.error(error)
